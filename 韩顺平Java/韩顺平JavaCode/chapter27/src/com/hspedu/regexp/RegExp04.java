4c38ebeee5f515e3fa7fd17ea30b270fccc8d98e
Recall <101963390+ShallowRecall@users.noreply.github.com>
2023-11-23 00:22:42 +0800

    2023-11-23 00:22:41

---
+++ 韩顺平Java/韩顺平JavaCode/chapter27/src/com/hspedu/regexp/RegExp04.java
@@ -0,0 +1,24 @@
+package com.hspedu.regexp;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author ZhouYu
+ * @version 1.0
+ * 选择匹配符
+ */
+public class RegExp04 {
+    public static void main(String[] args) {
+
+        String content = "hanshunping 韩 寒冷";
+
+        String regStr = "han|韩|寒";
+
+        Pattern pattern = Pattern.compile(regStr);
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()) {
+            System.out.println("找到：" + matcher.group(0));
+        }
+    }
+}
---
+++ 韩顺平Java/韩顺平JavaCode/chapter27/src/com/hspedu/regexp/Test.java
@@ -0,0 +1,14 @@
+package com.hspedu.regexp;
+
+/**
+ * @author ZhouYu
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        String str = "abcdef";
+
+        System.out.println(str.substring(0, 4));//abcd
+    }
+}
