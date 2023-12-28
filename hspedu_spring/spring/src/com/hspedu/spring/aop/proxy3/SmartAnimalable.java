b6596a1b360abd45efb5a0868f447efaac650385
Recall <101963390+ShallowRecall@users.noreply.github.com>
2023-12-28 14:38:21 +0800

    2023年12月28日14:37:57-AOP学习代码

---
+++ hspedu_spring/spring/src/com/hspedu/spring/aop/proxy3/SmartAnimalable.java
@@ -0,0 +1,13 @@
+package com.hspedu.spring.aop.proxy3;
+
+/**
+ * @author ZhouYu
+ * @version 1.0
+ * 接口
+ */
+public interface SmartAnimalable {
+    //求和
+    float getSum(float i,float j);
+    //求差
+    float getSub(float i,float j);
+}
