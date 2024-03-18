eaacbfc70afd12371706debf2fe3d24b0adf40f5
Recall <1989424334@qq.com>
2024-03-18 20:41:21 +0800

    Time:2024年03月18日20:40:33 task:@WebServlet注入Servlet

--- hspedu_springboot/springboot-usersys/src/main/java/com/hspedu/springboot/Application.java
+++ hspedu_springboot/springboot-usersys/src/main/java/com/hspedu/springboot/Application.java
@@ -2,11 +2,14 @@ package com.hspedu.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author ZhouYu
  * @version 1.0
  */
+// 要求扫描com.hspedu.springboot 包/子包下的原生方式注入的Servlet
+@ServletComponentScan(basePackages = "com.hspedu.springboot")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ hspedu_springboot/springboot-usersys/src/main/java/com/hspedu/springboot/servlet/Servlet_.java
@@ -0,0 +1,27 @@
+package com.hspedu.springboot.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author ZhouYu
+ * @version 1.0
+ * 解读
+ * 1. 通过继承 HttpServlet 来开发原生的Servlet
+ * 2. @WebServlet 标识将 Servlet_ 对象/bean注入到容器
+ * 3. urlPatterns = {"/servlet01","/servlet02"}，对servlet配置了url-pattern
+ * 4. 提示：注入的原生的Servlet_ 不会被spring-boot的拦截器拦截
+ * 5. 对于开发的原生的Servlet，需要使用 @ServletComponentScan指定要扫描的原生Servlet包
+ * ，才会注入到spring 容器.
+ */
+@WebServlet(urlPatterns = {"/servlet01","/servlet02"})
+public class Servlet_ extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("hello,Servlet_!");
+    }
+}
