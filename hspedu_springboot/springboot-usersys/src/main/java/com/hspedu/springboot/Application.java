9c8e577c38b189919e5f2513b7b33dd2bca4d9d1
Recall <1989424334@qq.com>
2024-03-18 17:25:34 +0800

    Time:2024年03月18日17:25:27 task:完成全局异常学习

--- hspedu_springboot/springboot-usersys/src/main/java/com/hspedu/springboot/Application.java
+++ hspedu_springboot/springboot-usersys/src/main/java/com/hspedu/springboot/Application.java
@@ -12,5 +12,7 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
+
+        System.out.println("hello");
     }
 }
