87731123595fb71c3a18062771aba66d60a2e554
Recall <1989424334@qq.com>
2024-04-08 16:15:54 +0800

    Time:2024年04月08日16:15:46 Task:sentinel流量控制学习完成 schedule:1-68

--- hspedu_springcloud/e-commerce-center/member-service-nacos-provider-10004/src/main/java/com/hspedu/springcloud/controller/MemberController.java
+++ hspedu_springcloud/e-commerce-center/member-service-nacos-provider-10004/src/main/java/com/hspedu/springcloud/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,6 +22,24 @@ public class MemberController {
     // 装配MemberService
     @Resource
     public MemberService memberService;
+
+    @GetMapping("/t1")
+    public Result t1() {
+        return Result.success("t1()执行..");
+    }
+
+    @GetMapping("/t2")
+    public Result t2() {
+        //让线程休眠1s，模拟执行时间为1s
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //输出线程信息
+        log.info("执行t2() 线程id={}",Thread.currentThread().getId());
+        return Result.success("t2()执行..");
+    }
 
     //添加方法/接口
 
@@ -39,15 +58,22 @@ public class MemberController {
     //使用url占位符+@PathVariable
     @GetMapping("/member/get/{id}")
     public Result getMemberById(@PathVariable("id") Long id, HttpServletRequest request) {
+   /* //通过?携带参数方式
+    @GetMapping(value = "/member/get",params = "id")
+    public Result getMemberById(Long id) {*/
 
         /*String color = request.getParameter("color");
         String address = request.getParameter("address");*/
-        /*try {
-            //模拟超时，休眠5秒
-            TimeUnit.MICROSECONDS.sleep(5000);
+        try {
+            //模拟超时，让线程休眠1秒
+            TimeUnit.MILLISECONDS.sleep(1000);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }*/
+            e.printStackTrace();
+        }
+
+        // 输出线程的情况
+        log.info("enter 10004 getMemberById 当前线程id={} 时间={}",
+                Thread.currentThread().getId(), new Date());
 
         Member member = memberService.queryMemberById(id);
 
