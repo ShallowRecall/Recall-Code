1ab3a79e880da1fed16727f49580a28c7f8a0a3e
ShallowRecall <1989424334@qq.com>
2024-04-27 19:55:58 +0800

    2024年4月27日19:55:32

--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,18 +1,36 @@
 package cn.itcast.order.service;
 
+
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+
     @Autowired
+    private UserClient userClient;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2.用Feign远程调用
+        User user = userClient.findById(order.getUserId());
+        // 3.封装user到order
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+
+    /*@Autowired
     private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
@@ -27,5 +45,5 @@ public class OrderService {
         order.setUser(user);
         // 4.返回
         return order;
-    }
+    }*/
 }
